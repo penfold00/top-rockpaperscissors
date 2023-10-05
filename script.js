a18076d85f02211a2bfc3c0ee98f7c0451df4e72
penfold00 <109156188+penfold00@users.noreply.github.com>
2023-10-05 11:01:58 +0200

    ui done

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ function getComputerChoice() {
   return choice;
 }
 
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+
+rockBtn.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+paperBtn.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
+
+/*
 function getPlayerSelection(prefix = "Game: ") {
   return prompt(prefix + "Rock Paper Scissors?");
-}
+} */
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
@@ -39,15 +54,54 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+let pointsPlayer = 0;
+let pointsComputer = 0;
+const pointsPlayerDisplay = document.querySelector("#pointsPlayerDisplay");
+const pointsComputerDisplay = document.querySelector("#pointsComputerDisplay");
+const resultText = document.querySelector(".result-text");
+
 function logResult(result, playerSelection, computerSelection) {
-  if (result == "win")
-    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-  else if (result == "lose")
-    console.log(`You lose! ${playerSelection} loses to ${computerSelection}.`);
-  else if (result == "tie")
-    console.log(`It's a tie! ${playerSelection} equals ${computerSelection}.`);
+  if (result == "win") {
+    resultText.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+    pointsPlayerDisplay.textContent = ++pointsPlayer;
+  } else if (result == "lose") {
+    resultText.textContent = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    pointsComputerDisplay.textContent = ++pointsComputer;
+  } else if (result == "tie") {
+    resultText.textContent = `It's a tie! ${playerSelection} equals ${computerSelection}.`;
+  }
+
+  checkFinal(pointsPlayer, pointsComputer);
 }
 
+function checkFinal(pointsPlayer, pointsComputer) {
+  if (pointsPlayer == 5) {
+    resultText.style.cssText = "background:green; font-weight:900;";
+    resultText.textContent = "Congrats, you win!";
+    setTimeout(() => {
+      gameReset();
+    }, 3000);
+  }
+  if (pointsComputer == 5) {
+    resultText.style.cssText = "background:red; font-weight:900;";
+
+    resultText.textContent = "Damnit, the machine wins.";
+    setTimeout(() => {
+      gameReset();
+    }, 3000);
+  }
+}
+function gameReset() {
+  pointsPlayer = 0;
+  pointsComputer = 0;
+  resultText.style.cssText = "";
+
+  resultText.textContent = "New round!";
+  pointsPlayerDisplay.textContent = "0";
+  pointsComputerDisplay.textContent = "0";
+}
+
+/*
 function game(rounds) {
   let pointsPlayer = 0;
   let pointsComputer = 0;
@@ -69,3 +123,4 @@ function game(rounds) {
 }
 
 game(5);
+*/
